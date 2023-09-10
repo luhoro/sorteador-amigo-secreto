20da257bb1d45dd65265153177a2d177a6ff2de2
luhoro <luisashorowitz@gmail.com>
2023-09-09 21:39:22 -0300

    Test: Base form interface

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <form action="">
+      <input type="text" placeholder='Insira os nomes dos participantes' />
+      <button disabled={true}>Adicionar</button>
+    </form>
+  )
+}
+
+export default Formulario
